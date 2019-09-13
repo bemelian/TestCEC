b3b67e86fe8548278fd8cf08b85a29e1da883abe
bemelian <47381116+bemelian@users.noreply.github.com>
2019-09-13 18:05:31 +0300

    Improving algorithm performance

--- FaceDR.py
+++ FaceDR.py
@@ -1,7 +1,6 @@
 import cv2
 import numpy as np
 import os
-from PIL import Image
 
 path = 'faces/'
 cascadePath = "haarcascade_frontalface_default.xml"
@@ -15,11 +14,11 @@ def get_images(path):
     labels = []
     j = 0
     for image_path in image_paths:
-        gray = Image.open(image_path).convert('L')
-        image = np.array(gray, 'uint8')
+        gray = cv2.imread(image_path)
+        image = cv2.cvtColor(gray, cv2.COLOR_BGR2GRAY)
         subject_number = int(os.path.basename(image_path).split(".")[0].replace("subject", ""))
 
-        faces = faceCascade.detectMultiScale(image, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+        faces = faceCascade.detectMultiScale(image, scaleFactor=1.15, minNeighbors=5, minSize=(40, 40))
         for (x, y, w, h) in faces:
             images.append(image[y: y + h, x: x + w])
             # cv2.imwrite('frontalfaces/subject' + str(subject_number) + '_' + str(j) + '.jpg', image[y: y + h, x: x + w])
@@ -47,7 +46,7 @@ while (cap.isOpened()):
     ret, frame = cap.read()
     if ret == True:
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        face = faceCascade.detectMultiScale(frame, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+        face = faceCascade.detectMultiScale(frame, scaleFactor=1.15, minNeighbors=5, minSize=(40, 40))
         for (x, y, w, h) in face:
             number_predicted, conf = recognizer.predict(gray[y: y + h, x: x + w])
             if conf < mconf:
